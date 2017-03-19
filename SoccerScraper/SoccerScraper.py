25f73760f7acf424309ac6b3ac931be343fe6329
Thomas Primer <thomas.primer@gmail.com>
2017-03-19 20:03:00 +1030

    Gets list of prem teams

--- SoccerScraper/SoccerScraper.py
+++ SoccerScraper/SoccerScraper.py
@@ -7,5 +7,19 @@ APIkey = {'X-Auth-Token':'30bd6dd569b04133ad67fe61d34fb76f'}
 
 print("Making request")
 response = requests.get(url,headers=APIkey)
-jData = json.loads(response.text)
-print(type(jData))
+leagueData = json.loads(response.text)
+for key,value in leagueData.items():
+    if(type(value) is not dict):
+        print("{:}: {:}".format(key,value))
+    else:
+        for k,v in value.items():
+            print("    {:}: {:}".format(k,v))
+
+teamsurl = leagueData['_links']['teams']['href']
+print("Making request")
+response = requests.get(teamsurl,headers=APIkey)
+teamData = json.loads(response.text)
+print(teamData['teams'][0]['shortName'][0])
+
+for team in teamData['teams']:
+    print("{:}".format(team['name']))
